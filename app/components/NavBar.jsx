53da453f4abcef056e1871b8746d53fe6ea39513
Joanne Kim <joannekim806@gmail.com>
2025-11-28 18:21:43 -0700

    added transitions and fixed image sizing on home page

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -44,7 +44,7 @@ export default function NavBar({ title }) {
         <div className="flex justify-between w-full bg-[#F5BAA7] items-center p-4">
             <div className="text-white font-bold text-xl">
                 <Link href="/">
-                    <button className="hover:underline cursor-pointer">{title}</button>
+                    <button className="cursor-pointer">{title}</button>
                 </Link>
             </div>
             <ul className="flex gap-6 list-none items-center">
@@ -52,7 +52,7 @@ export default function NavBar({ title }) {
                     <li key={btn.name}>
                         <Link href={btn.route}>
                             <button
-                                className={`text-white font-semibold p-2 ${btn.name === "Login" ? "w-20 h-12 bg-[#D17368] rounded hover:underline cursor-pointer" : "hover:underline cursor-pointer"
+                                className={`text-white font-semibold p-2 ${btn.name === "Login" ? "w-20 h-12 bg-[#D17368] rounded hover:bg-[#b5645b] transition duration-300 cursor-pointer" : "w-20 h-12 rounded hover:bg-[#d99c89] transition duration-300 cursor-pointer"
                                     }`}
                             >
                                 {btn.name}
--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,28 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* NavBar */}
-      {/* <NavBar
-        title="Recipe Book"
-      /> */}
 
-      {/* Main Content */}
       <main className="flex flex-row flex-grow p-8 gap-12 bg-[#FFF2DF]">
-        {/* <div className="flex flex-col flex-grow p-8 gap-12"> */}
-        <section className="w-1/2 flex-grow">
-          <img className="w-full h-full object-fill" src="/black.png" alt="" />
+        <section className="w-1/2 flex justify-center items-center">
+          <div className="w-full max-w-md aspect-square">
+            <img className="w-full h-full object-fill" src="/black.png" alt="" />
+          </div>
         </section>
-        {/* </div> */}
-
+        
         <section className="w-1/2 justify-center items-center flex flex-col">
           <h3 className="!text-[#D17368]">A Recipe Book for All Your Needs</h3>
-          <p className="text-7xl font-bold text-[#D17368]">RECIPE BOOK</p>
+          <h1 className="text-7xl font-bold text-[#D17368]">RECIPE BOOK</h1>
           <p className="mb-6">
             Discover a world of culinary delights with our Recipe Book app! Whether you're a seasoned chef or a kitchen novice, our app offers a diverse collection of recipes to suit every taste and occasion. From quick weeknight dinners to elaborate gourmet meals, find step-by-step instructions, ingredient lists, and cooking tips to help you create delicious dishes with ease.
           </p>
-          <section className="flex flex-row gap-10">
+          <section className="flex flex-row flex-wrap justify-center items-center gap-10">
             <Link href="/register">
-              <button className="bg-[#D17368] text-white font-semibold w-51 h-12 rounded hover:underline cursor-pointer">
+              <button className="bg-[#D17368] text-white font-semibold w-51 h-12 rounded hover:bg-[#b5645b] transition duration-300 cursor-pointer">
                 Register
               </button>
             </Link>
             <Link href="/login">
-              <button className="bg-[#FFF2DF] text-[#D17368] font-semibold w-51 h-12 rounded border-3 border-[#D17368] hover:underline cursor-pointer">
+              <button className="bg-[#FFF2DF] text-[#D17368] font-semibold w-51 h-12 rounded border-3 border-[#D17368] hover:bg-[#ffe8c7] transition duration-300 cursor-pointer">
                 Login
               </button>
             </Link>
